5d0b80c40e092d2a0320136a2f04db7ca40aa62e
mehrank22 <mehran.mkaa@gmail.com>
2024-11-11 23:19:53 +0330

    7 commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "810155885"
+    serviceWorkerVersion: "2291373939"
   }
 });
